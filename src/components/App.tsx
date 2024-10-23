65245252318faa45322bd7a6ad7fca4ee7527461
ahui <782492184@qq.com>
2024-10-24 03:17:56 +0800

    update

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -122,9 +122,8 @@ export const App: FC = () => {
     backButton.on('click', () => {
       navigate(-1)
     })
-    setTimeout(() => {
-      postEvent('web_app_open_link', {url: 'https://www.baidu.com'})
-    }, 5000);
+    // postEvent('web_app_open_link', {url: 'https://www.baidu.com'})
+
     postEvent('web_app_set_header_color', { color_key: 'bg_color' });
 
     disSwipe()
